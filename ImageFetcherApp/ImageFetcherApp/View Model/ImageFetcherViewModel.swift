25b6594eec183ec0ecd451d9af5e1b12f4390b3e
@a.dj.kumar <a.dj.kumar@accenture.com>
2020-06-15 10:21:40 +0530

    Image fetcher class moved to ViewModel

--- ImageFetcherApp/ImageFetcherApp/View Model/ImageFetcherViewModel.swift
+++ ImageFetcherApp/ImageFetcherApp/View Model/ImageFetcherViewModel.swift
@@ -16,3 +16,40 @@ class ImageFetcherViewModel {
         self.title = model.title
     }
 }
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+class CustomImageView: UIImageView {
+    var imageUrlString: String?
+    public func imageFromURL(urlString: String) {
+        imageUrlString = urlString
+        image = nil
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.frame = CGRect.init(x: 50, y: 50, width: self.frame.size.width, height: self.frame.size.height)
+        activityIndicator.startAnimating()
+        if let imageFromCache = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = imageFromCache
+            return
+        }
+        if self.image == nil{
+            self.addSubview(activityIndicator)
+        }
+        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
+            
+            if error != nil {
+                print(error ?? "No Error")
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                activityIndicator.stopAnimating()
+                let imageToCache = UIImage(data: data!)
+                if self.imageUrlString == urlString {
+                    self.image = imageToCache
+                }
+                if imageToCache != nil{
+                    imageCache.setObject(imageToCache!, forKey: urlString as AnyObject)
+                }
+                activityIndicator.removeFromSuperview()
+            })
+        }).resume()
+    }
+}
